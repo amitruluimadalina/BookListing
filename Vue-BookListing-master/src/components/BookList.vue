fca25cf95bbcfa40911caa7147f8f60aa3b98523
amitruluimadalina <104475104+amitruluimadalina@users.noreply.github.com>
2022-05-16 05:11:26 +0300

    Add files via upload

---
+++ Vue-BookListing-master/src/components/BookList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1> {{ title }} </h1>
+        <ul>
+            <div v-for="book in books" :key="book">
+                <book-item :book="book"></book-item>
+            </div>
+        </ul>
+        <book-form @addBook='appendBook'></book-form>
+    </div>
+</template>
+
+<script>
+ import BookItem from './BookItem';
+ import BookForm from './BookForm';
+    export default {
+        name: 'BookList',
+        components: {BookItem, BookForm},
+        methods: {
+            appendBook: function(bookTitle, bookAuthor){
+                this.books.push({ title: bookTitle, author: bookAuthor });
+            }
+        },
+        data() {
+            return {
+                title: "All Books",
+                books: [
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
+            };
+        },
+    };
+</script>
+
+<style lang="css">
+    h1, h2{
+        font-weight: normal;
+    }
+    ul{
+        list-style-type: none;
+        padding: 0;
+    }
+</style>
